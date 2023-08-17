998eebafe10619756c2724c69ec700f82c45cdfc
Renier Vargas <renier1989@gmail.com>
2023-08-16 20:28:12 -0500

    Class117 - Creating the Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Form, Formik } from "formik"
 import { Input } from "./components/Input"
+import { Button } from "./components/Button"
 
 function App() {
   const handleSubmit = () =>{}
   return (
   <div className='flex h-screen items-center justify-center bg-slate-300 '>
-    <div className='bg-white py-20 px-4 rounded-lg w-full border-t-4 border-t-purple-300 shadow-lg
+    <div className='bg-white py-12 px-4 rounded-lg w-full border-t-4 border-t-purple-300 shadow-lg
     mx-10 
     sm:mx-[100px] 
     md:mx-[400px] 
@@ -24,6 +25,7 @@ function App() {
           <Input name="contribution" label="Anual Contribution"></Input>
           <Input name="years" label="Years"></Input>
           <Input name="rate" label="Estimated Interest"></Input>
+          <Button />
         </Form>
       </Formik>
     </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+
+
+export const Button = () => {
+  return (
+    <div className="flex items-center justify-center pt-5">
+        <button className=" bg-purple-200 px-5 py-2 w-52 rounded-md font-semibold text-xl text-slate-700 hover:text-white hover:shadow-xl hover:bg-purple-400 duration-500 ease-in-out transition "> Calculate</button>
+    </div>
+  )
+}
