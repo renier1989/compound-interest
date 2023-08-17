68e34d4f6d109031bb75e89cb15bcc548385dda3
Renier Vargas <renier1989@gmail.com>
2023-08-16 19:06:03 -0500

    Class115 - Creating the Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ function App() {
 
   return (
   <div className='flex h-screen items-center justify-center bg-slate-300 '>
-    <div className='bg-white py-20 px-4 rounded-lg w-full mx-10 sm:mx-[100px] md:mx-[400px] xl:mx-[600px]'>
+    <div className='bg-white py-20 px-4 rounded-lg w-full border-t-4 border-t-purple-300 shadow-lg
+    mx-10 
+    sm:mx-[100px] 
+    md:mx-[400px] 
+    xl:mx-[600px]'>
       Hello world!
     </div>
   </div>
